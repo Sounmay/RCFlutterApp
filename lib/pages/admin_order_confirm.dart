9de3988ea84661dd7ae29d5b23047d5d6fa2f054
naiyar.2000 <naiyarimam.2000@gmail.com>
2020-09-18 01:45:55 +0530

    orders wala reh gaya tha

--- lib/pages/admin_order_confirm.dart
+++ lib/pages/admin_order_confirm.dart
@@ -1,190 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter_spinkit/flutter_spinkit.dart';
-// import 'package:rcapp/models/user.dart';
-// import 'package:rcapp/services/database.dart';
-// import 'package:provider/provider.dart';
-
-// class ConfirmedOrderDetails {
-//   String name;
-//   String number;
-//   int total;
-//   String address;
-// }
-
-// class AdminOrder extends StatefulWidget {
-//   @override
-//   _AdminOrderState createState() => _AdminOrderState();
-// }
-
-// class _AdminOrderState extends State<AdminOrder> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamProvider<List<Orders>>.value(
-//       value: DatabaseService().orders,
-//       child: Scaffold(
-//         appBar: AppBar(
-//           title: Text('Orders'),
-//           backgroundColor: Colors.deepOrange,
-//         ),
-//         body: NewOrderList(),
-//       ),
-//     );
-//   }
-// }
-
-// class NewOrderList extends StatefulWidget {
-//   @override
-//   NewOrderListState createState() => NewOrderListState();
-// }
-
-// class NewOrderListState extends State<NewOrderList> {
-//   changeConfirmation(bool isConfirmed, String number) async {
-//     try {
-//       if (isConfirmed) {
-//         await Firestore.instance
-//             .collection("confirmedOrders")
-//             .document(number)
-//             .delete();
-//       } else {
-//         await Firestore.instance
-//             .collection("confirmedOrders")
-//             .document(number)
-//             .updateData({"isConfirmed": !isConfirmed});
-//       }
-//       Navigator.pushReplacementNamed(context, '/adminorder');
-//     } catch (e) {
-//       print(e.toString());
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     int orderNo = 110;
-//     final _orderList = Provider.of<List<Orders>>(context) ?? [];
-//     return ListView.builder(
-//       shrinkWrap: true,
-//       itemCount: _orderList.length,
-//       itemBuilder: (_, index) {
-//         orderNo += index;
-//         return ListTile(
-//           contentPadding: EdgeInsets.all(10),
-//           title: Column(
-//             children: <Widget>[
-//               SizedBox(height: 20.0),
-//               Container(
-//                 alignment: Alignment.topLeft,
-//                 height: 420.0,
-//                 width: 370.0,
-//                 margin: new EdgeInsets.only(left: 20.0, right: 20.0),
-//                 decoration: BoxDecoration(
-//                   color: Colors.tealAccent,
-//                   borderRadius: BorderRadius.all(Radius.circular(20.0)),
-//                 ),
-//                 child: Column(
-//                   children: <Widget>[
-//                     Row(
-//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                       children: <Widget>[
-//                         Text(
-//                           "Order Number : " + "$orderNo",
-//                           style:
-//                               TextStyle(color: Colors.deepOrange, fontSize: 22),
-//                         ),
-//                         Text("Aug 4th 2020"),
-//                       ],
-//                     ),
-//                     Column(
-//                       children: <Widget>[
-//                         Row(
-//                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                           children: <Widget>[
-//                             Padding(
-//                               padding: const EdgeInsets.all(10),
-//                               child: Text(
-//                                 "Name : " + "${_orderList[index].name}",
-//                                 style: TextStyle(
-//                                     color: Colors.black, fontSize: 15),
-//                               ),
-//                             ),
-//                           ],
-//                         ),
-//                         Row(
-//                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                           children: <Widget>[
-//                             Padding(
-//                               padding: const EdgeInsets.all(10),
-//                               child: Text(
-//                                 "Personal No. : " +
-//                                     "${_orderList[index].number}",
-//                                 style: TextStyle(
-//                                     color: Colors.black, fontSize: 15),
-//                               ),
-//                             ),
-//                           ],
-//                         ),
-//                         Row(
-//                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                           children: <Widget>[
-//                             Flexible(
-//                               child: Text(
-//                                 "Address : " + "${_orderList[index].address}",
-//                                 style: TextStyle(
-//                                     color: Colors.black, fontSize: 15),
-//                               ),
-//                             ),
-//                           ],
-//                         ),
-//                       ],
-//                     ),
-//                     SizedBox(height: 30),
-//                     Row(
-//                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                       children: <Widget>[
-//                         Flexible(
-//                           child: Text(
-//                             "Cost : ₹" +
-//                                 "${_orderList[index].total} " +
-//                                 "${_orderList[index].item}" +
-//                                 " : " +
-//                                 "${_orderList[index].qty}",
-//                             style: TextStyle(color: Colors.black, fontSize: 22),
-//                           ),
-//                         ),
-//                         FlatButton(
-//                           onPressed: () {
-//                             Navigator.pushNamed(context, '/orderdetails');
-//                           },
-//                           shape: new RoundedRectangleBorder(
-//                               borderRadius: new BorderRadius.circular(10.0)),
-//                           child: Text('View'),
-//                           color: Colors.white,
-//                         ),
-//                         FlatButton(
-//                           onPressed: () {
-//                             changeConfirmation(_orderList[index].isConfirmed,
-//                                 _orderList[index].number);
-//                           },
-//                           shape: new RoundedRectangleBorder(
-//                               borderRadius: new BorderRadius.circular(10.0)),
-//                           child: Text(_orderList[index].isConfirmed
-//                               ? 'Confirm'
-//                               : 'Delete'),
-//                           color: Colors.amber,
-//                         ),
-//                       ],
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ],
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -225,8 +38,10 @@ class _AdminOrderState extends State<AdminOrder> {
   }
 
   void initialData() async {
-    var result =
-        await Firestore.instance.collection('confirmedOrders').orderBy('_date', descending: true).getDocuments();
+    var result = await Firestore.instance
+        .collection('confirmedOrders')
+        .orderBy('_date', descending: true)
+        .getDocuments();
     result.documents.forEach((res) {
       setState(() {
         orders.add(res.data);
@@ -282,7 +97,7 @@ class _AdminOrderState extends State<AdminOrder> {
                       height: 190.0,
                       width: 370.0,
                       decoration: BoxDecoration(
-                          color: Colors.blue[50],
+                          color: Colors.white,
                           borderRadius: BorderRadius.all(Radius.circular(12.0)),
                           border: Border.all(color: Colors.grey)),
                       child: Column(
@@ -397,14 +212,14 @@ class _AdminOrderState extends State<AdminOrder> {
                                       padding: EdgeInsets.symmetric(
                                           horizontal: 5, vertical: 4),
                                       decoration: BoxDecoration(
-                                          color: Colors.white,
+                                          color: Colors.red[700],
                                           border:
                                               Border.all(color: Colors.black),
                                           borderRadius:
                                               BorderRadius.circular(5)),
                                       child: Text('View',
                                           style: TextStyle(
-                                            color: Colors.black,
+                                            color: Colors.white,
                                           )),
                                     ),
                                   ),
@@ -419,7 +234,7 @@ class _AdminOrderState extends State<AdminOrder> {
                                         padding: EdgeInsets.symmetric(
                                             horizontal: 5, vertical: 4),
                                         decoration: BoxDecoration(
-                                            color: Colors.deepOrange[400],
+                                            color: Colors.deepOrange,
                                             border:
                                                 Border.all(color: Colors.black),
                                             borderRadius:
@@ -433,7 +248,8 @@ class _AdminOrderState extends State<AdminOrder> {
                                   ] else ...[
                                     InkWell(
                                       onTap: () {
-                                        deleteOrder(index, orders[index]["_date"]);
+                                        deleteOrder(
+                                            index, orders[index]["_date"]);
                                       },
                                       child: Container(
                                         padding: EdgeInsets.symmetric(
@@ -686,12 +502,6 @@ class _AdminOrderDetailsState extends State<AdminOrderDetails> {
                               ),
                             ]),
                         SizedBox(height: 20),
-                        // Container(
-                        //     width: double.infinity,
-                        //     child: FlatButton(
-                        //         onPressed: () {},
-                        //         color: Colors.deepOrange,
-                        //         child: Text('Confirm')))
                       ],
                     ),
                   ),
